2bea3b4ccf2938b9a91249a41db7dad2050e8922
dino <dino@websegera.my>
2018-06-20 01:26:13 -0400

    page slug validation

--- inc/functions.php
+++ inc/functions.php
@@ -6,8 +6,13 @@ function validate_slug($str) {
   // each array entry is an special char allowed
   // besides the ones from ctype_alnum
   $allowed = array("-", "_");
+  $prevented_slug = array('404','index');
 
-  if ( ctype_alnum( str_replace($allowed, '', $str ) ) ) {
+  if ( 
+      ctype_alnum( str_replace($allowed, '', $str ) ) // must match alphanum, dash & underscore only
+      && (!in_array($str, $prevented_slug)) // not in prevented words
+      && (strlen($str) > 4) // must be over 4 characters
+  ) {
     return true;
   } 
     return false;
